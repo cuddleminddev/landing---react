2bf403cd2bdf2bdba0b327936657f1209143f409
cuddleminddev <cuddleminddev@gmail.com>
2025-07-11 14:00:16 +0530

    app section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,11 @@
+import App from "./home/app";
 import Journey from "./home/journey";
 
 export default function Home() {
   return (
-    <Journey />
+    <>
+      <Journey />
+      <App />
+    </>
   );
 }
---
+++ app/routes/home/app.tsx
@@ -0,0 +1,27 @@
+import "./styles/app.css"
+
+export default function App() {
+    return (
+        <div className="app-container">
+            <div className="app-left">
+                <img src="/home2.png" alt="logo" />
+            </div>
+            <div className="app-right">
+                <div className="app-content">
+                    <h4>Get Cuddlemind App</h4>
+                    <p>Available on all platforms. Start your mental wellness journey today.</p>
+                </div>
+                <div className="store-images">
+                    <div className="app-playstore">
+                        <img src="/playstore.png" alt="playstore" />
+                        <p>Get it from Play store</p>
+                    </div>
+                    <div className="app-appstore">
+                        <img src="/appstore.png" alt="appstore" />
+                        <p>Available in App store</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
